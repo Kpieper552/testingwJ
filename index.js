2398bf0c13ae3d5e2094e075242f063eb03c13eb
Kpieper552 <k.pieper@novi-education.nl>
2021-01-28 10:57:07 +0100

    'testjestkarina'

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-// write function beneath for testjest
-// function getSize(width, length)
+function getSize(width, length) {
+    return width * length;
+}
+const Size = getSize(85, 1.70)
+
+console.log(Size)
 
 
 
